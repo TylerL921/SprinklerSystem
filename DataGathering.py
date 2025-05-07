b7ba21bf5a9d964a7e902d4b982073ba63219121
Tyler <tylerlin921@gmail.com>
2025-05-07 16:12:07 -0400

    Gathering data from open meteo. TODO: Send formated dataframe to postgres

--- DataGathering.py
+++ DataGathering.py
@@ -5,40 +5,76 @@ import pandas as pd
 import requests_cache
 from retry_requests import retry
 
+# User Information - Adjust Accordingly
+latLong = 44.099, -79.133 # Latitude and Longitude
+hourlyD = ["temperature_2m", "precipitation"]
+dailyD = ["precipitation_sum", "temperature_2m_max"]
+timezone = "America/New_York"
+
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
 # Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-	"latitude": 52.52,
-	"longitude": 13.41,
-	"hourly": "temperature_2m"
+	"latitude": latLong[0],
+	"longitude": latLong[1],
+    "daily": dailyD,
+	"hourly": hourlyD,
+    "timezone": timezone,
+    "forecast_days": 3
 }
+
+# Gather the data from open meteo
 responses = openmeteo.weather_api(url, params=params)
+response = responses[0] # Note: Must change if querying for multiple locations
 
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
-# Process hourly data. The order of variables needs to be the same as requested.
+
+# Process hourly data.
 hourly = response.Hourly()
-hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 
-hourly_data = {"date": pd.date_range(
+# Time - Database Key
+dataH = {"date": pd.date_range(
 	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
 	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
 	freq = pd.Timedelta(seconds = hourly.Interval()),
 	inclusive = "left"
 )}
 
-hourly_data["temperature_2m"] = hourly_temperature_2m
+# Other Data
+for i in range(len(hourlyD)):
+    dataH[hourlyD[i]] = hourly.Variables(i).ValuesAsNumpy()
+    
 
-hourly_dataframe = pd.DataFrame(data = hourly_data)
-print(hourly_dataframe)
+# Create Dataframe
+hourly_dataframe = pd.DataFrame(data = dataH)
+hourly_long = hourly_dataframe.melt(id_vars="date", var_name="tag", value_name="value")
+print(hourly_long)
+
+
+
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+
+# Date
+dataD = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+
+# Other Data
+for i in range(len(dailyD)):
+    dataD[dailyD[i]] = daily.Variables(i).ValuesAsNumpy()
+
+daily_dataframe = pd.DataFrame(data = dataD)
+daily_long = daily_dataframe.melt(id_vars="date", var_name="tag", value_name="value")
+print(daily_long)
+
+
+# Connect to Postgres
