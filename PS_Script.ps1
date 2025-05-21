e985eb88770ae70a3ee508def14ac8cb6c118794
Tyler <tylerlin921@gmail.com>
2025-05-21 10:26:58 -0400

    Created SprinklerCom

---
+++ PS_Script.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\Tyler\Documents\Sprinkler
+(& C:\ProgramData\anaconda3\shell\condabin\conda-hook.ps1) ; (conda activate Sprinkler5)
+python -u "c:\Users\Tyler\Documents\Sprinkler\DataGathering.py"
---
+++ SprinklerCom.py
@@ -0,0 +1,7 @@
+# Imports
+import psycopg2
+import pandas as pd
+
+# Parameters
+spRate = 0 # Sprinkler Rate (ml/s)
+desiredHyd = 0 # Desired Hydration (ml)
